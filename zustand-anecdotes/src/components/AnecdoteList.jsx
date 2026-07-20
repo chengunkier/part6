68870fd03cced6ec060876086f86f325e285c716
chengunkier <inabamicheal97@gmail.com>
2026-07-20 15:02:11 +0300

    completed exercise 6.5: anecdotes, step4

--- zustand-anecdotes/src/components/AnecdoteList.jsx
+++ zustand-anecdotes/src/components/AnecdoteList.jsx
@@ -4,9 +4,11 @@ const AnecdoteList = () => {
   const anecdotes = useAnecdoteStore((state) => state.anecdotes)
   const voteAnecdote = useAnecdoteStore((state) => state.voteAnecdote)
 
+  const sortedAnecdotes = anecdotes.toSorted((a, b) => b.votes - a.votes)
+
   return (
     <div>
-      {anecdotes.map((anecdote) => (
+      {sortedAnecdotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
