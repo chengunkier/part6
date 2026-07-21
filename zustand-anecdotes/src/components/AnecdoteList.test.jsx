d69e5a9a90571ccd96cfe5d1c95108f66329547a
chengunkier <inabamicheal97@gmail.com>
2026-07-21 11:29:09 +0300

    completed exercise 6.13 anecdotes, step12

---
+++ zustand-anecdotes/src/components/AnecdoteList.test.jsx
@@ -0,0 +1,27 @@
+import { describe, test, expect, beforeEach } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import useAnecdoteStore from '../store'
+import AnecdoteList from './AnecdoteList'
+
+describe('<AnecdoteList />', () => {
+  beforeEach(() => {
+    useAnecdoteStore.setState({
+      anecdotes: [
+        { content: 'low votes', id: '1', votes: 1 },
+        { content: 'high votes', id: '2', votes: 10 },
+        { content: 'medium votes', id: '3', votes: 5 },
+      ],
+      filter: '',
+    })
+  })
+
+  test('anecdotes are rendered in descending order by votes', () => {
+    render(<AnecdoteList />)
+
+    const contentElements = screen
+      .getAllByText(/votes$/)
+      .map((el) => el.textContent)
+
+    expect(contentElements).toEqual(['high votes', 'medium votes', 'low votes'])
+  })
+})
