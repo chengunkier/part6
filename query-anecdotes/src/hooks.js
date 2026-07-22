121333f5365ab94208239cc4eee6842131f0601d
chengunkier <inabamicheal97@gmail.com>
2026-07-22 10:19:57 +0300

    completed Exercise 6.20

--- query-anecdotes/src/hooks.js
+++ query-anecdotes/src/hooks.js
@@ -1,5 +1,6 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import { getAnecdotes, createAnecdote, updateAnecdote } from './requests'
+import { useNotificationDispatch } from './NotificationContext'
 
 export const useAnecdotes = () => {
   return useQuery({
@@ -9,24 +10,35 @@ export const useAnecdotes = () => {
   })
 }
 
+const showNotification = (dispatch, message) => {
+  dispatch({ type: 'SET_NOTIFICATION', payload: message })
+  setTimeout(() => {
+    dispatch({ type: 'CLEAR_NOTIFICATION' })
+  }, 5000)
+}
+
 export const useCreateAnecdoteMutation = () => {
   const queryClient = useQueryClient()
+  const dispatch = useNotificationDispatch()
 
   return useMutation({
     mutationFn: createAnecdote,
-    onSuccess: () => {
+    onSuccess: (newAnecdote) => {
       queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+      showNotification(dispatch, `anecdote '${newAnecdote.content}' created`)
     },
   })
 }
 
 export const useVoteAnecdoteMutation = () => {
   const queryClient = useQueryClient()
+  const dispatch = useNotificationDispatch()
 
   return useMutation({
     mutationFn: updateAnecdote,
-    onSuccess: () => {
+    onSuccess: (votedAnecdote) => {
       queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+      showNotification(dispatch, `anecdote '${votedAnecdote.content}' voted`)
     },
   })
 }
